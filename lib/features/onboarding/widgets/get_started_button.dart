6b080619f152ab07c2b4b8847c5975f65704627c
C0de871 <muhammadaydi7@gmail.com>
2025-08-12 20:29:09 +0300

    - added images & svgs folders to assets - added flutter_svg & added flutter_screenutil - made a file for doc app client - added navigation extention - added routing folder that contain router and routes file - added colors and text styles - added empty login screen - added onboarding screen

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_course/core/theme/colors.dart';
+
+import '../../../core/routing/routes.dart';
+import '../../../core/theme/styles.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pushNamed(context, Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: WidgetStateProperty.all(const Size(double.infinity, 50)),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        ),
+      ),
+      child: Text("Get Started", style: TextStyles.font16WhiteSemiBold),
+    );
+  }
+}
